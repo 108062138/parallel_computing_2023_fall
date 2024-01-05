41f322f8ebd61140f4e8ca97296c91be3fba8caa
108062138 <poyuwu17@gmail.com>
2024-01-05 11:38:44 +0800

    append multi-gpu

--- final_project/cuda/run_cuda.sh
+++ final_project/cuda/run_cuda.sh
@@ -1,2 +1,5 @@
 make
-srun -n 1 --gres=gpu:1 ./dct_cuda ../src/Barbara.jpg ../out/Barbara.png ../out/Barbara_cuda.png
+# srun -n 1 --gres=gpu:1 ./dct_cuda ../src/Barbara.jpg ../out/Barbara.png ../out/Barbara_cuda.png
+srun -N1 -n1 -c2 --gres=gpu:2 ./dct_cuda_many_gpu ../src/Barbara.jpg         ../out/Barbara.png ../out/Barbara_cuda.png
+srun -N1 -n1 -c2 --gres=gpu:2 ./dct_cuda_many_gpu ../src/Mona_Lisa_1024.jpg  ../out/ms.png      ../out/ms_dct_omp.png
+srun -N1 -n1 -c2 --gres=gpu:2 ./dct_cuda_many_gpu ../src/Mona_Lisa_2048.jpg  ../out/ms.png      ../out/ms_dct_cuda.png
